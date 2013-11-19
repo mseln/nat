cf57e412e42308725a7e95523d4bb0ba3f2ff6ac
mseln <mange.selin@gmail.com>
2013-11-19 21:49:14 +0100

    Added area calculation function for arbitrary polygon

---
+++ geometry/area.cpp
@@ -0,0 +1,12 @@
+// area.cpp
+// Calculate the area of an arbitrary polygon
+// <vector> and "geometry.cpp" must be included
+
+template <class T>
+int area(vector<Vector<T> > v){
+    int area = 0;
+    for(int i = 0; i < v.size()-1; i++)
+        area += (v[i] % v[i+1]).z;
+    area += (v[v.size()-1] % v[0]).z;
+    return area;
+}
